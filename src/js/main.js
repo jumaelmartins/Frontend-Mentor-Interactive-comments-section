4bde84a727b5660afc2baa83d781419709286591
jumaelmartins <jumaelmartins@gmail.com>
2023-03-11 20:19:52 -0300

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-import '../style/main.scss'
+import '../style/main.scss';
+
+
+
+let itens;
+let id;
+
+const getItensLS = () => JSON.parse(localStorage.getItem("itens")) ?? [];
+const setItensLS = () => localStorage.setItem("itens", JSON.stringify(itens));
+
